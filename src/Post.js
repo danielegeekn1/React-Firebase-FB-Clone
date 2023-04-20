dd86c495d8382bcaf3abe590c9c2c638033a5d12
Daniele Basile <daniele.basile.job@gmail.com>
2023-04-20 12:26:47 +0200

    added like, comment and share button to post section

--- src/Post.js
+++ src/Post.js
@@ -2,6 +2,10 @@ import { Avatar } from "@material-ui/core";
 import React from "react";
 //import css
 import "./post.css";
+//import icon from material ui
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import ShareIcon from "@mui/icons-material/Share";
 const Post = ({ profilePic, img, username, timestamp, message }) => {
   return (
     <div className="post">
@@ -18,6 +22,20 @@ const Post = ({ profilePic, img, username, timestamp, message }) => {
         </div>
         <div className="post_img">
           <img src={img} alt="" />
+        </div>
+        <div className="post_options">
+          <div className="post_option">
+            <ThumbUpIcon />
+            <p>Like</p>
+          </div>
+          <div className="post_option">
+            <ChatBubbleIcon />
+            <p>Comment</p>
+          </div>
+          <div className="post_option">
+            <ShareIcon />
+            <p>Share</p>
+          </div>
         </div>
       </div>
     </div>
